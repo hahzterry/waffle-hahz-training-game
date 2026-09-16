02185ecbeeb9d774dd55b86439e2e6eda1709fdf
Wizard of Hahz <info@blkluv.org>
2026-09-16 10:52:41 -0400

    Refactor Title scene text and button properties

--- src/game/scenes/Title.js
+++ src/game/scenes/Title.js
@@ -1,5 +1,3 @@
-// src/game/scenes/Title.js
-
 import Phaser from 'phaser';
 
 export default class Title extends Phaser.Scene {
@@ -10,9 +8,9 @@ export default class Title extends Phaser.Scene {
     create() {
         const { width, height } = this.scale;
 
-        this.cameras.main.setBackgroundColor('#151515');
+        this.cameras.main.setBackgroundColor('#121212');
 
-        this.add.text(width / 2, 125, 'WAFFLE HAHZ', {
+        this.add.text(width / 2, 110, 'WAFFLE HAHZ', {
             fontFamily: 'Arial Black',
             fontSize: '76px',
             color: '#ffffff',
@@ -20,58 +18,85 @@ export default class Title extends Phaser.Scene {
             strokeThickness: 8
         }).setOrigin(0.5);
 
-        this.add.text(width / 2, 205, 'THE HOUSE ALTERNATIVE', {
+        this.add.text(width / 2, 195, 'THE HOUSE ALTERNATIVE', {
             fontFamily: 'Arial Black',
-            fontSize: '25px',
+            fontSize: '24px',
             color: '#ffb000',
             letterSpacing: 4
         }).setOrigin(0.5);
 
-        this.add.text(width / 2, 290, 'WAFFLE HAHZ GAME', {
+        this.add.text(width / 2, 285, 'WAFFLE HAHZ GAME', {
             fontFamily: 'Arial Black',
             fontSize: '42px',
             color: '#ff4d1c'
         }).setOrigin(0.5);
 
-        this.add.text(width / 2, 350, 'TRAINING DAY', {
+        this.add.text(width / 2, 330, 'TRAINING DAY', {
             fontFamily: 'Arial Black',
-            fontSize: '24px',
+            fontSize: '22px',
             color: '#ffffff'
         }).setOrigin(0.5);
 
-        const play = this.add.rectangle(
+        const playButton = this.add.rectangle(
             width / 2,
-            475,
-            330,
-            90,
+            455,
+            340,
+            85,
             16
         )
         .setFillStyle(0xffb000)
-        .setStrokeStyle(5, 0xffffff)
-        .setInteractive({ useHandCursor: true });
-
-        this.add.text(width / 2, 475, 'START TRAINING', {
-            fontFamily: 'Arial Black',
-            fontSize: '30px',
-            color: '#111111'
-        }).setOrigin(0.5);
-
-        play.on('pointerover', () => {
-            play.setScale(1.05);
+        .setStrokeStyle(4, 0xffffff)
+        .setInteractive({
+            useHandCursor: true
         });
 
-        play.on('pointerout', () => {
-            play.setScale(1);
+        const playText = this.add.text(
+            width / 2,
+            455,
+            'START TRAINING',
+            {
+                fontFamily: 'Arial Black',
+                fontSize: '28px',
+                color: '#121212'
+            }
+        ).setOrigin(0.5);
+
+        playButton.on('pointerover', () => {
+            playButton.setFillStyle(0xffffff);
+            playText.setColor('#121212');
+            playButton.setScale(1.04);
         });
 
-        play.on('pointerdown', () => {
+        playButton.on('pointerout', () => {
+            playButton.setFillStyle(0xffb000);
+            playText.setColor('#121212');
+            playButton.setScale(1);
+        });
+
+        playButton.on('pointerdown', () => {
             this.scene.start('GrillInstructions');
         });
 
-        this.add.text(width / 2, 620, 'Master the waffles. Master the chicken. Master the rush.', {
-            fontFamily: 'Arial',
-            fontSize: '18px',
-            color: '#aaaaaa'
-        }).setOrigin(0.5);
+        this.add.text(
+            width / 2,
+            590,
+            'Master the waffle. Master the chicken. Master the rush.',
+            {
+                fontFamily: 'Arial',
+                fontSize: '18px',
+                color: '#999999'
+            }
+        ).setOrigin(0.5);
+
+        this.add.text(
+            width / 2,
+            height - 30,
+            'NASHVILLE HOT  •  ATL PEACH  •  LEMON PEPPER  •  RED VELVET  •  BACON BOURBON',
+            {
+                fontFamily: 'Arial',
+                fontSize: '13px',
+                color: '#666666'
+            }
+        ).setOrigin(0.5);
     }
 }

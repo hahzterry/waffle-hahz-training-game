71c6c643adf193cec8eb8b829f47caa095cc6e1e
Wizard of Hahz <info@blkluv.org>
2026-09-16 10:45:18 -0400

    Update Title.js

--- src/game/scenes/Title.js
+++ src/game/scenes/Title.js
@@ -1,48 +1,77 @@
-import { Scene } from 'phaser';
+// src/game/scenes/Title.js
 
-export class Title extends Scene
-{
-    constructor ()
-    {
+import Phaser from 'phaser';
+
+export default class Title extends Phaser.Scene {
+    constructor() {
         super('Title');
     }
 
-    preload ()
-    {
-        this.load.setPath('assets');
-        
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
+    create() {
+        const { width, height } = this.scale;
 
-        this.load.image('server-selection', 'server-selection.png');
-        this.load.image('grill-selection', 'grill-selection.png');}
+        this.cameras.main.setBackgroundColor('#151515');
 
-    create ()
-    {
-        this.add.image(512, 200, 'background');
-        this.add.image(512, 300, 'logo').setDepth(100);
+        this.add.text(width / 2, 125, 'WAFFLE HAHZ', {
+            fontFamily: 'Arial Black',
+            fontSize: '76px',
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8
+        }).setOrigin(0.5);
 
-        let serverSelection = this.add.image(this.scale.width /2 - 150, 650, 'server-selection').setDepth(100);
-        serverSelection
-            .setScale((0.5))
-            .setInteractive(
-                {cursor: 'pointer'
-                 }
-            );
+        this.add.text(width / 2, 205, 'THE HOUSE ALTERNATIVE', {
+            fontFamily: 'Arial Black',
+            fontSize: '25px',
+            color: '#ffb000',
+            letterSpacing: 4
+        }).setOrigin(0.5);
 
+        this.add.text(width / 2, 290, 'WAFFLE HAHZ GAME', {
+            fontFamily: 'Arial Black',
+            fontSize: '42px',
+            color: '#ff4d1c'
+        }).setOrigin(0.5);
 
-        let grillSelection = this.add.image(this.scale.width /2 + 150, 650, 'grill-selection').setDepth(100);
-        grillSelection
-            .setScale((0.5))
-            .setInteractive(
-                {cursor: 'pointer'
-                 }
-            );
-        grillSelection.on('pointerdown', () => {
-            // Change to the scene with the key 'GameScene'
-            this.scene.start('GrillInstructions'); 
-            });
+        this.add.text(width / 2, 350, 'TRAINING DAY', {
+            fontFamily: 'Arial Black',
+            fontSize: '24px',
+            color: '#ffffff'
+        }).setOrigin(0.5);
+
+        const play = this.add.rectangle(
+            width / 2,
+            475,
+            330,
+            90,
+            16
+        )
+        .setFillStyle(0xffb000)
+        .setStrokeStyle(5, 0xffffff)
+        .setInteractive({ useHandCursor: true });
+
+        this.add.text(width / 2, 475, 'START TRAINING', {
+            fontFamily: 'Arial Black',
+            fontSize: '30px',
+            color: '#111111'
+        }).setOrigin(0.5);
+
+        play.on('pointerover', () => {
+            play.setScale(1.05);
+        });
+
+        play.on('pointerout', () => {
+            play.setScale(1);
+        });
+
+        play.on('pointerdown', () => {
+            this.scene.start('GrillInstructions');
+        });
+
+        this.add.text(width / 2, 620, 'Master the waffles. Master the chicken. Master the rush.', {
+            fontFamily: 'Arial',
+            fontSize: '18px',
+            color: '#aaaaaa'
+        }).setOrigin(0.5);
     }
-
-
 }

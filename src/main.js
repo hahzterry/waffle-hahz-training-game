e1313b2de3d947c1eab44f6fef8f72c2a032ecd0
Wizard of Hahz <info@blkluv.org>
2026-09-16 10:50:23 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,40 @@
-// src/game/main.js
-
 import Phaser from 'phaser';
-import Title from './scenes/Title';
-import GrillInstructions from './scenes/Grill_Instructions';
-import Grill from './scenes/Grill';
-import Results from './scenes/Results';
+
+import Title from './scenes/Title.js';
+import GrillInstructions from './scenes/Grill_Instructions.js';
+import Grill from './scenes/Grill.js';
+import Results from './scenes/Results.js';
 
 const config = {
     type: Phaser.AUTO,
+
     width: 1280,
     height: 720,
+
     parent: 'game',
-    backgroundColor: '#111111',
+
+    backgroundColor: '#121212',
+
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1280,
         height: 720
     },
+
+    render: {
+        antialias: true,
+        pixelArt: false,
+        roundPixels: false
+    },
+
     physics: {
         default: 'arcade',
         arcade: {
             debug: false
         }
     },
+
     scene: [
         Title,
         GrillInstructions,
